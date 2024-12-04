0f0d06a0f57ba2f594fe8c5fedcd3d774cf63f68
MiguelGH6586 <miguel.agh97@gmail.com>
2024-12-03 21:51:45 -0500

    connection with mongo DB and register user added

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,20 +1,23 @@
 import { Controller } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { MessagePattern } from '@nestjs/microservices';
+import { MessagePattern, Payload, RpcException } from '@nestjs/microservices';
+import { LoginUserDto, RegisterUserDto } from './dto';
+import { log } from 'console';
+import { catchError } from 'rxjs';
 
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @MessagePattern('auth.register.user')
-  registerUser() {
-    return 'register user';
-  //  return this.authService.registerUser(data);
+  registerUser(@Payload() registerUserDto: RegisterUserDto) {
+    //return registerUserDto;
+    return this.authService.registerUser(registerUserDto);
   }
 
   @MessagePattern('auth.login.user')
-  loginUser() {
-    return 'login user';
+  loginUser(@Payload() loginUserDto: LoginUserDto) {
+    return loginUserDto;
   //  return this.authService.loginUser(data);
   }
 
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
+import { PrismaClient } from '@prisma/client';
+import { RegisterUserDto } from './dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger('AuthService');
+
+  onModuleInit() {
+    this.$connect();
+    this.logger.log('Connected to MongoDB');
+  }
+
+  async registerUser(registerUserDto: RegisterUserDto) {
+    try {
+      const user = await this.user.findUnique({
+        where: {
+          email: registerUserDto.email,
+        },
+      });
+
+      if (user) {
+        throw new RpcException({
+          status: 400,
+          message: 'User already exists',
+        });
+      }
+
+      const newUser = await this.user.create({
+        data: {
+          email: registerUserDto.email,
+          name: registerUserDto.name,
+          password: registerUserDto.password,
+        },
+      });
+
+      return { user: newUser, token: "ABC" };
+    } catch (error) {
+      throw new RpcException({
+        status: 400,
+        message: error.message,
+      });
+    }
+  }
+}
